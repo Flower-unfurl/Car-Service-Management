c1c3904fb6b158aa6ae12f84d8d19280ef63a2ac
bunmasv2 <thaibao5335@gmail.com>
2026-04-07 22:22:31 +0700

    Fix logic

--- client/src/services/dispatchApi.js
+++ client/src/services/dispatchApi.js
@@ -121,6 +121,10 @@ const dispatchApi = {
         const res = await api.get(`/ticket/guest/${qrToken}/invoice`);
         return unwrapData(res);
     },
+    getAppointmentsByPhone: async (phone) => {
+        const res = await api.get(`/appointment/lookup/${phone}`);
+        return unwrapData(res);
+    },
 };
 
 export default dispatchApi;
